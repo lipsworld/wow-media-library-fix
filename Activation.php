7f9ab79658b8e067028fae25efe6f3bcd190312c
wowpress-host <39159589+wowpress-host@users.noreply.github.com>
2018-05-17 15:43:08 +0200

    spaces to tabs

--- Activation.php
+++ Activation.php
@@ -4,11 +4,11 @@ namespace WowMediaLibraryFix;
 
 class Activation {
 	static public function deactivate() {
-	    Util::status_delete();
-	    Util::status_unreferenced_basenames_delete();
+		Util::status_delete();
+		Util::status_unreferenced_basenames_delete();
 
-	    $wp_upload_dir = wp_upload_dir();
-	    $log_to_file_filename = $wp_upload_dir['basedir'] .
+		$wp_upload_dir = wp_upload_dir();
+		$log_to_file_filename = $wp_upload_dir['basedir'] .
 			DIRECTORY_SEPARATOR . 'media-library.log';
 		@unlink( $log_to_file_filename );
 	}
